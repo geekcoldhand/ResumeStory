f4c0df98baa4af453e97d469c5ac966d63470b7c
geekcoldhand <horatiousaharris@gmail.com>
2025-11-04 16:39:21 -0500

    added functionality for progress tracker

--- src/components/Storyline/Storybook.jsx
+++ src/components/Storyline/Storybook.jsx
@@ -1,5 +1,5 @@
 import "./Storybook.css";
-import { useRef } from "react";
+import { use, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
@@ -18,23 +18,23 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 const Storybook = () => {
 	const rootRef = useRef(null);
 	const progressBarRef = useRef(null);
+	const timelineRef = useRef(null);
 
 	useGSAP(
 		() => {
+			const container = rootRef.current;
+			console.log(container);
 			ScrollTrigger.create({
-				scroller: rootRef.current,
-				start: "top top",
-				end: "bottom bottom",
-				scrub: true,
-				markers: false,
-				onUpdate: (self) => {
-					gsap.to(progressBarRef.current, {
-						width: `${self.progress * 100}%`,
-						duration: 0.1,
-					});
+				scroller: container,
+				trigger: timelineRef.current,
+				start: "top top", // Start tracking when the top of the container hits the top of the viewport
+				end: "bottom top", // Stop tracking when the bottom of the container hits the top of the viewport
+				onUpdate: ({ progress }) => {
+					console.log(`Scroll Progress: ${Math.round(progress * 100)}%`);
 				},
+				scrub: true,
 			});
-
+			// more code below
 			const clouds = gsap.utils.toArray(".cloud");
 			clouds.forEach((cloud, index) => {
 				const direction = index % 2 === 0 ? 150 : -150;
@@ -130,34 +130,36 @@ const Storybook = () => {
 			<div className="progress-container">
 				<div className="progress-bar" ref={progressBarRef}></div>
 			</div>
-			<Hero />
-			<Clouds />
-			<About />
-			{/* <Geogrophy /> */}
-			<Education />
-			<Carousel />
-			<Professional />
-			<Today />
-			<footer className=" footer-scene center">
-				<button
-					className="pinstripe"
-					style={{
-						fontSize: "0.5rem",
-						padding: "0.5rem",
-						borderRadius: "0.5rem",
-						border: "none",
-						backgroundColor: "var(--c-compliment)",
-						color: "white",
-						marginBlock: "1rem",
-					}}
-					onClick={() =>
-						rootRef.current.scrollTo({ top: 0, behavior: "smooth" })
-					}
-				>
-					Back To Top
-				</button>
-				<small className="pinstripe">Storybook</small>
-			</footer>
+			<div className="timeline" ref={timelineRef}>
+				<Hero />
+				<Clouds />
+				<About />
+				{/* <Geogrophy /> */}
+				<Education />
+				<Carousel />
+				<Professional />
+				<Today />
+				<footer className=" footer-scene center">
+					<button
+						className="pinstripe"
+						style={{
+							fontSize: "0.5rem",
+							padding: "0.5rem",
+							borderRadius: "0.5rem",
+							border: "none",
+							backgroundColor: "var(--c-compliment)",
+							color: "white",
+							marginBlock: "1rem",
+						}}
+						onClick={() =>
+							rootRef.current.scrollTo({ top: 0, behavior: "smooth" })
+						}
+					>
+						Back To Top
+					</button>
+					<small className="pinstripe">Storybook</small>
+				</footer>
+			</div>
 		</div>
 	);
 };
